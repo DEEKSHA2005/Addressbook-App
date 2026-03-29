c305496c680073f3fb25c6598ef561b562184784
Deeksha <smdeeksha2005@gmail.com>
2026-03-29 16:12:41 +0530

    [Deeksha]: SEC3-UC2: Implemented logging using Lombok @Slf4j in service layer

--- src/main/java/com/bridgelabz/addressbook/service/AddressBookService.java
+++ src/main/java/com/bridgelabz/addressbook/service/AddressBookService.java
@@ -2,11 +2,13 @@ package com.bridgelabz.addressbook.service;
 
 import com.bridgelabz.addressbook.dto.AddressBookDTO;
 import com.bridgelabz.addressbook.model.AddressBookModel;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
 import java.util.Map;
 
+@Slf4j
 @Service
 public class AddressBookService {
 
@@ -15,31 +17,43 @@ public class AddressBookService {
 
     // CREATE
     public AddressBookModel create(AddressBookDTO dto) {
+        log.info("Creating new address: {}", dto);
+
         AddressBookModel model = new AddressBookModel();
         model.setId(idCounter++);
         model.setName(dto.getName());
         model.setCity(dto.getCity());
 
         addressBook.put(model.getId(), model);
+
+        log.info("Created address with ID: {}", model.getId());
         return model;
     }
 
     // GET ALL
     public Map<Integer, AddressBookModel> getAll() {
+        log.info("Fetching all addresses");
         return addressBook;
     }
 
-    // GET BY ID (IMPROVED)
+    // GET BY ID
     public AddressBookModel getById(int id) {
+        log.info("Fetching address with id: {}", id);
+
         if (!addressBook.containsKey(id)) {
+            log.error("Address not found with id: {}", id);
             throw new RuntimeException("Address not found with id: " + id);
         }
+
         return addressBook.get(id);
     }
 
-    // UPDATE (IMPROVED)
+    // UPDATE
     public AddressBookModel update(int id, AddressBookDTO dto) {
+        log.info("Updating address with id: {}", id);
+
         if (!addressBook.containsKey(id)) {
+            log.error("Cannot update. Address not found with id: {}", id);
             throw new RuntimeException("Cannot update. Address not found with id: " + id);
         }
 
@@ -47,14 +61,21 @@ public class AddressBookService {
         model.setName(dto.getName());
         model.setCity(dto.getCity());
 
+        log.info("Updated address with id: {}", id);
         return model;
     }
 
-    // DELETE (IMPROVED)
+    // DELETE
     public void delete(int id) {
+        log.info("Deleting address with id: {}", id);
+
         if (!addressBook.containsKey(id)) {
+            log.error("Cannot delete. Address not found with id: {}", id);
             throw new RuntimeException("Cannot delete. Address not found with id: " + id);
         }
+
         addressBook.remove(id);
+
+        log.info("Deleted address with id: {}", id);
     }
 }
